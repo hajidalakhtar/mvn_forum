cab57a22043c21b76d83208aa8bb47768ec80f91
Hajidalakhtar <hajidalakhtar@gmail.com>
2023-05-15 17:04:28 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -124,7 +124,7 @@ const Home = (props) => {
             className=" w-100 h-screen"
             style={{
               backgroundSize: "cover",
-              backgroundImage: `url(https://ik.imagekit.io/jh2scbhjww/Template_BGArtboard_2-100_CNi5T6JyA.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1676546046435)`,
+              backgroundImage: `url(https://ik.imagekit.io/jh2scbhjww/WhatsApp_Image_2023-05-15_at_12.46.29_Qcu6CLt6H.jpeg?updatedAt=1684141149810)`,
             }}
         >
           <br />
